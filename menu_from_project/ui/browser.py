6e1572c78b312673bd9bc36d4f0b8e6a77079f97
jmkerloch <jean-marie.kerloch@oslandia.com>
2024-10-04 16:06:53 +0200

    feat(layer load): refactor for add load option

--- menu_from_project/ui/browser.py
+++ menu_from_project/ui/browser.py
@@ -127,7 +127,13 @@ class ProjectCollection(QgsDataCollectionItem):
         children = []
         for child in self.project_menu_config.root_group.childs:
             if isinstance(child, MenuLayerConfig):
-                children.append(LayerItem(parent=self, layer_config=child))
+                children.append(
+                    LayerItem(
+                        parent=self,
+                        layer_config=child,
+                        group_name=self.project_menu_config.root_group.name,
+                    )
+                )
             elif isinstance(child, MenuGroupConfig):
                 children.append(GroupItem(parent=self, group_config=child))
         return children
@@ -158,7 +164,14 @@ class GroupItem(QgsDataCollectionItem):
         children = []
         for child in self.group_config.childs:
             if isinstance(child, MenuLayerConfig):
-                children.insert(0, LayerItem(parent=self, layer_config=child))
+                children.insert(
+                    0,
+                    LayerItem(
+                        parent=self,
+                        layer_config=child,
+                        group_name=self.group_config.name,
+                    ),
+                )
             elif isinstance(child, MenuGroupConfig):
                 children.insert(0, GroupItem(parent=self, group_config=child))
         return children
@@ -167,15 +180,20 @@ class GroupItem(QgsDataCollectionItem):
 class LayerItem(QgsDataItem):
     """QgsDataItem for layer"""
 
-    def __init__(self, parent: QgsDataItem, layer_config: MenuLayerConfig):
+    def __init__(
+        self, parent: QgsDataItem, layer_config: MenuLayerConfig, group_name: str
+    ):
         """Constructor for a QgsDataItem to display layer configuration
 
         :param parent: parent
         :type parent: QgsDataItem
         :param layer_config: layer configuration
         :type layer_config: MenuLayerConfig
+        :param group_name: group name
+        :type group_name: str
         """
         self.layer_config = layer_config
+        self.group_name = group_name
         self.path = os.path.join(parent.path, layer_config.name)
         QgsDataItem.__init__(
             self, QgsDataItem.Custom, parent, layer_config.name, self.path
@@ -221,11 +239,4 @@ class LayerItem(QgsDataItem):
 
     def addLayer(self) -> None:
         """Add layer to current QGIS project"""
-        LayerLoad().loadLayer(
-            self.layer_config.filename,
-            self.layer_config.filename,
-            self.layer_config.layer_id,
-            None,
-            self.layer_config.visible,
-            self.layer_config.expanded,
-        )
+        LayerLoad().load_layer(self.layer_config, self.group_name)
